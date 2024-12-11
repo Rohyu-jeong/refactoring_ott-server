f42f01eb1cf2617dd1b73f1dfba560beedb78c75
Rohyj <yacheku@gmail.com>
2024-12-11 10:10:04 +0900

    feat: Spring Security와 JWT 인증 구현
    - SecurityConfig 추가: 인증 및 권한 부여를 위한 Spring Security 설정
    - JWTFilter 추가: HTTP 요청에서 JWT 토큰 검증 및 인증 로직
    - JWTUtil 추가: JWT 토큰 생성, 파싱, 만료 여부 확인 로직
    - WebConfig 추가: CORS 설정 구성

---
+++ src/main/java/com/ott/ott_service/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.ott.ott_service.config;
+
+import com.ott.ott_service.security.JWTFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+    private final UserDetailsService userDetailsService;
+    private final JWTFilter jwtFilter;
+
+    // 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 인증 제공자 설정
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+//
+    // SecurityFilterChain 설정
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/login", "/member/add").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/member/changePassword/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/memberprofile/changePassword/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    // AuthenticationManager 빈 설정 (인증 관리자 설정)
+    @Bean
+    public AuthenticationManager authenticationManagerBean(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(authenticationProvider())
+                .build();
+    }
+}
