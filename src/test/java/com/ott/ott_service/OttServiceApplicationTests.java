165e1c6d96ffc53cd6380f66c40d540a87c05f84
Rohyj <yacheku@gmail.com>
2024-12-14 12:00:27 +0900

    fix: 기본 테스트 파일 누락 추가 (첫 커밋 때 넣었어야 했음...)

---
+++ src/test/java/com/ott/ott_service/OttServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ott.ott_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
