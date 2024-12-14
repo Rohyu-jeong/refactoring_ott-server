8f1729bf8003053a437b081378f440a9d7a8a8d9
Rohyj <yacheku@gmail.com>
2024-12-14 12:03:06 +0900

    fix: 누락된 OttServiceApplication.java 파일 추가

---
+++ src/main/java/com/ott/ott_service/OttServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ott.ott_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttServiceApplication.class, args);
+	}
+
+}
